c30835ec35673dde6c48e668f4a82ae1ca24eca8
JamesClarke01 <81041174+JamesClarke01@users.noreply.github.com>
2022-11-23 15:41:05 +0000

    First Screen Change Working

---
+++ app/src/main/java/com/example/giveget/Give1.java
@@ -0,0 +1,16 @@
+package com.example.giveget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Give1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_give1);
+    }
+
+
+}
