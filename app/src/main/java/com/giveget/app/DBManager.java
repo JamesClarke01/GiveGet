a09124aa89baa8107bff7561839dadfb0d952ce0
James Clarke <jamesclarkedublin@gmail.com>
2022-11-28 14:16:18 +0000

    Foodlisting Database Insertion Implemented

---
+++ app/src/main/java/com/giveget/app/DBManager.java
@@ -0,0 +1,106 @@
+package com.giveget.app;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DBManager {
+
+    Context context;
+    private DBHelper dbHelper;
+    private SQLiteDatabase database;
+
+    public DBManager(Context context) {this.context = context;}
+
+    public DBManager open() throws SQLException
+    {
+        dbHelper = new DBHelper(context);  //instantiate the db helper class
+        database = dbHelper.getWritableDatabase();  //gets database, and if doesn't already exist, creates it
+        return this;
+    }
+
+    public void close() {dbHelper.close();}
+
+    public long insertFoodlisting(String name, String expiryDate, int amount, String image, String description)
+    {
+        ContentValues insertingValues = new ContentValues();
+        insertingValues.put(dbHelper.KEY_FOODLISTING_NAME, name);
+        insertingValues.put(dbHelper.KEY_FOODLISTING_EXPIRY, expiryDate);
+        insertingValues.put(dbHelper.KEY_FOODLISTING_AMOUNT, amount);
+        insertingValues.put(dbHelper.KEY_FOODLISTING_IMAGE, image);
+        insertingValues.put(dbHelper.KEY_FOODLISTING_DESC, description);
+
+        return database.insert(dbHelper.TABLE_NAME_FOODLISTING, null, insertingValues);
+    }
+
+    public Cursor getAllFoodlistings() throws SQLException{
+        return database.rawQuery("SELECT * FROM" + dbHelper.TABLE_NAME_FOODLISTING, null);
+    }
+
+}
+
+
+class DBHelper extends SQLiteOpenHelper {
+    //Code structure for this class loosely inspired by example found at: https://guides.codepath.com/android/Local-Databases-with-SQLiteOpenHelper
+
+    //database information
+    public static final String DATABASE_NAME = "GiveGetDB";
+    public static final int DATABASE_VERSION = 1;
+
+    //table names
+    public static final String TABLE_NAME_FOODLISTING = "FoodListing";
+    public static final String TABLE_NAME_USER = "User";
+
+    //foodlisting table columns
+    private static final String KEY_FOODLISTING_ID = "id";
+    public static final String KEY_FOODLISTING_NAME = "name";
+    public static final String KEY_FOODLISTING_EXPIRY = "expiry";
+    public static final String KEY_FOODLISTING_AMOUNT = "amount";
+    public static final String KEY_FOODLISTING_IMAGE = "image";
+    public static final String KEY_FOODLISTING_DESC = "description";
+    //need to add foreign keys here
+
+
+
+    public DBHelper(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        //create table foodlisting
+        final String CREATE_TABLE_FOODLISTING =
+                "CREATE TABLE " + TABLE_NAME_FOODLISTING +
+                        "(" +
+                            KEY_FOODLISTING_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                            KEY_FOODLISTING_NAME + " TEXT," +
+                            KEY_FOODLISTING_EXPIRY + " TEXT," +
+                            KEY_FOODLISTING_AMOUNT + " INTEGER," +
+                            KEY_FOODLISTING_IMAGE + " TEXT," +
+                            KEY_FOODLISTING_DESC + " TEXT" +
+                        ")";
+
+        db.execSQL(CREATE_TABLE_FOODLISTING);
+
+    }
+
+    //if version number is greater than old version number when this is called, recreate the database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion != newVersion)
+        {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_FOODLISTING);
+
+            onCreate(db);
+        }
+
+    }
+}
--- app/src/main/java/com/giveget/app/GiveActivity1.java
+++ app/src/main/java/com/giveget/app/GiveActivity1.java
@@ -10,10 +10,24 @@ import com.example.giveget.R;
 
 public class GiveActivity1 extends AppCompatActivity {
 
+    DBManager dbManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_give1);
+
+        //instantiate dbmanager
+        dbManager = new DBManager(this);
+    }
+
+    public void createFoodListing(View view)
+    {
+        dbManager.open(); //connect to database
+
+        dbManager.insertFoodlisting("Apples", "30/11/2022", 3, "apples.png", "3 Fresh Apples");
+
+        //dbManager.close();
     }
 
 
