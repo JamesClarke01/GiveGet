bbfbaa524d0ce05c7bc53ec2ee3c04ad664a2690
James Clarke <jamesclarkedublin@gmail.com>
2022-11-29 23:01:56 +0000

    Basic Listview implemented on GetActivity1

--- app/src/main/java/com/giveget/app/GetActivity1.java
+++ app/src/main/java/com/giveget/app/GetActivity1.java
@@ -3,26 +3,35 @@ package com.giveget.app;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.giveget.R;
 
 public class GetActivity1 extends AppCompatActivity {
+
+    DBManager dbManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get1);
 
+        dbManager = new DBManager(this); //instantiate the dbManager
+
         //display back button in action bar
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+
+
+        updateFoodList();
+
+
     }
-
-
-
 
     //function for action bar back button implementation
     @Override
@@ -37,6 +46,26 @@ public class GetActivity1 extends AppCompatActivity {
         {
             return false;
         }
+    }
+
+    public void updateFoodList()
+    {
+        dbManager.open();
+
+        //Cursor foodlistingCursor = dbManager.getAllFoodlistings();
+
+        String names[] = {"apples", "onions", "oranges"};  //array
+
+        ListView foodListView = findViewById(R.id.foodListView);  //list view
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, names );
+
+        foodListView.setAdapter(arrayAdapter);
+
+
+        dbManager.close();
+
+
     }
 
 }
