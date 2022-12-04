252bbff9ed7477dcd8fbebc501152661a8238b25
David <davite3@gmail.com>
2022-12-04 20:55:56 +0000

    better browser

--- app/src/main/java/com/giveget/app/MainActivity.java
+++ app/src/main/java/com/giveget/app/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.giveget.R;
